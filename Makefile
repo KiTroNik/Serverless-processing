396099b99cbfbdd9559118b4bfea696cd24d612d
Jakub Tomala <jakub.tomala@stxnext.pl>
2023-04-22 11:19:30 +0200

    Function for fetching

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 MYPY_DIRS := $(shell find functions layer -type d -maxdepth 1 -mindepth 1 | xargs)
+ARTIFACTS_DIR ?= build
 
 .PHONY: test
 test:
@@ -12,3 +13,22 @@ mypy: $(MYPY_DIRS)
 develop:
 	python -m pip install --editable .
 	python -m pip install -U -r requirements-dev.txt
+
+.PHONY: build
+build:
+	rm -rf dist || true
+	python -m build -w
+
+.PHONY: build_layer
+build_layer: build
+	rm -rf "$(ARTIFACTS_DIR)/python" || true
+	mkdir -p "$(ARTIFACTS_DIR)/python"
+	python -m pip install -r requirements.txt -t "$(ARTIFACTS_DIR)/python"
+	python -m pip install dist/*.whl -t "$(ARTIFACTS_DIR)/python"
+
+.PHONY: package_layer
+package_layer: build build_layer
+	cd "$(ARTIFACTS_DIR)"; zip -rq ../layer.zip python
+
+.PHONY: build-ServerlessProjectLayer
+build-ServerlessProjectLayer: build build_layer
--- functions/fetch/handler.py
+++ functions/fetch/handler.py
@@ -1,8 +1,42 @@
-import json
+import io
+import csv
+
+import boto3
+import requests
+from requests import Response
+from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+s3 = boto3.client("s3")
+app = APIGatewayRestResolver()
 
 
-def lambda_handler(event, context):
+@app.post("/process_fact")
+def fetch_for_processing():
+    request_data: dict = app.current_event.json_body
+
+    fact: Response = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
+    fact.raise_for_status()
+    request_data["fact"] = fact.json()["text"]
+
+    stream = io.StringIO()
+    writer = csv.DictWriter(stream, fieldnames=["fact", "email"])
+    writer.writeheader()
+    writer.writerow(request_data)
+    csv_string_object = stream.getvalue()
+    s3.put_object(Bucket="bucket_name", Key=f"{fact.json()['id']}.csv", Body=csv_string_object)
+
     return {
-        "statusCode": 200,
-        "body": json.dumps("Hello from lambda")
+        "fact": fact.json(),
+        "email": request_data["email"]
     }
+
+
+def _fetch_fact() -> Response:
+    result: Response = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
+    result.raise_for_status()
+    return result
+
+
+def handler(event: dict, context: LambdaContext) -> dict:
+    return app.resolve(event, context)
